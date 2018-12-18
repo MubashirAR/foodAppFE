ef2f6baafed06ac8d9fa55625cb46100067afac7
Mubashir Reshamwala <mubresham@gmail.com>
2018-12-18 12:16:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'foodAppFE';
+  title = 'Welcome to the food(ie) app!';
 }
