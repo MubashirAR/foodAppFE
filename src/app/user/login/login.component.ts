909dee985f5862b559d427da28546e8a1ac5c4b7
Mubashir Reshamwala <mubresham@gmail.com>
2019-01-05 18:58:37 +0530

    added sample login register code

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  submitValue = 'submit';
+  profileGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  onSubmit = () => {this.http.get('http://localhost:3000/user', { params: this.profileGroup.value }).subscribe<Response>(
+    response => {
+      if (response.status === 'success') {
+        localStorage.setItem('userID', response.data._id);
+        this.router.navigate(['/']);
+      } else {
+        console.log('failure');
+      }
+    }); }
 
   ngOnInit() {
   }
 
+}
+class Response {
+  status: string;
+  data: Object;
 }
