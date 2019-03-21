bd546ef640a3141407a29dd43bf4b35d68d8c32b
Mubashir Reshamwala <mubresham@gmail.com>
2019-03-21 08:01:22 +0530

    integrated new register UI with apis

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -16,8 +17,25 @@ export class RegisterComponent implements OnInit {
     mobile : new FormControl(''),
     password : new FormControl(''),
   });
+  address = new FormGroup({
+    address_line_1: new FormControl(''),
+    address_line_2: new FormControl(''),
+    address_city: new FormControl(''),
+    address_state: new FormControl(''),
+    address_zip: new FormControl(''),
+    address_country_id: new FormControl(''),
+  })
+  profileGroupRestaurant = new FormGroup({
+    name : new FormControl(''),
+    last_name : new FormControl(''),
+    email : new FormControl(''),
+    phone : new FormControl(''),
+    password : new FormControl(''),
+    address:this.address
+  });
   submitValue = 'Submit!';
-  constructor(private http: HttpClient) { }
+  failedMessage= '';
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.getUsers();
@@ -28,7 +46,26 @@ export class RegisterComponent implements OnInit {
   }
   onSubmit = () => {
     this.http.post('http://localhost:3000/registerUser', this.profileGroup.value)
-            .subscribe(data => console.log(data));
+            .subscribe((response:any) => {
+              if(response.data.status == 'success') {
+                localStorage.setItem('userID', response.data._id);
+                this.router.navigate(['/']);
+              } else {
+                this.failedMessage = response.data;
+              }
+            });
+  }
+  onSubmitRestaurant = () => {
+    console.log("testttt");
+    this.http.post('http://localhost:3000/registerRestaurant', this.profileGroupRestaurant.value)
+            .subscribe((response:any) => {
+              if(response.data.status == 'success') {
+                localStorage.setItem('userID', response.data._id);
+                this.router.navigate(['/']);
+              } else {
+                this.failedMessage = response.data;
+              }
+            });
   }
 
 }
